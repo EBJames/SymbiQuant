7dc771d138bf4093ad0437f6fcb18b8eeb078d4e
EBJames <30508317+EBJames@users.noreply.github.com>
2021-04-20 16:13:34 -0400

    Deleted spaces on trainer_augmentation.py

--- trainer_augmentation.py
+++ trainer_augmentation.py
@@ -13,13 +13,13 @@ class RandomScale(T.Augmentation):
         self.min_scale = min_scale
         self.max_scale = max_scale
 
-     def get_transform(self, image):
+    def get_transform(self, image):
         old_h, old_w = image.shape[:2]
         sacle = np.random.uniform(self.min_scale, self.max_scale)
         new_h, new_w = int(old_h * sacle), int(old_w * sacle)
         return T.ResizeTransform(old_h, old_w, new_h, new_w)
 
- class BuchneraTrainer(DefaultTrainer):
+class BuchneraTrainer(DefaultTrainer):
     @classmethod
     def build_train_loader(cls, cfg):
         return build_detection_train_loader(cfg, mapper=DatasetMapper(cfg, is_train=True, augmentations=[
